3436246588ce6e528d8cccd9aa58e95921e828b5
john mgosi <mgosijohn083@gmail.com>
2026-09-14 00:03:57 -0700

    Fix: Resolve all git merge conflicts in composer, artisan, and user model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,11 +9,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-<<<<<<< HEAD
     /** @use HasFactory<UserFactory> */
-=======
-    /** @use HasFactory<\Database\Factories\UserFactory> */
->>>>>>> a45ebbfdcd09c358913bc2cf7dd54bcbf2254134
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -37,7 +33,6 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-<<<<<<< HEAD
     /**
  * The roles that belong to the user.
  */
@@ -46,21 +41,6 @@ public function roles()
     return $this->belongsToMany(\App\Models\Role::class, 'role_user')
                 ->withPivot('start_date', 'end_date')
                 ->withTimestamps();
-=======
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
->>>>>>> a45ebbfdcd09c358913bc2cf7dd54bcbf2254134
 }
 
     }
